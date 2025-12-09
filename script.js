23c56067fdc2ef1662b5d11c268218bc391dcfe0
ahabbany <ahabbany@gmail.com>
2025-12-09 14:59:15 +0700

    inisialisasi projek

---
+++ script.js
@@ -0,0 +1,16 @@
+// Memberi tahu mahasiswa bahwa script berhasil di-load
+alert('Selamat datang di Website Profil UMKM Anda!');
+// Menampilkan informasi variabel di console
+const namaUMKM = 'Kopi Nusantara';
+console.log('Nama UMKM:', namaUMKM);
+// Demonstrasi variabel dan operator
+let produk = 3;
+console.log('Jumlah produk saat ini:', produk);
+produk += 2;
+console.log('Setelah penambahan:', produk);
+document.getElementById("langganan").addEventListener("change", function () 
+{ 
+  if (this.checked) { 
+    alert("Terima kasih telah berlangganan newsletter!"); 
+  } 
+}); 
